e1fb4fdbaa0a326d47325e79d504444d30b1792a
emilyschur <emilyschur.dev@gmail.com>
2022-05-10 11:19:24 -0700

    daily coding challenge

---
+++ 2022-05-10/app.js
@@ -0,0 +1,31 @@
+//URL--https://www.codewars.com/kata/5a360620f28b82a711000047
+
+//INSTRUCTIONS
+/*
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+//SOLUTION
+function defineSuit(card) {
+    if (card.includes('♣')) {
+        return 'clubs';
+    } else if (card.includes('♦')) {
+        return 'diamonds';
+    } else if (card.includes('♥')) {
+        return 'hearts';
+    } else {
+        return 'spades';
+    }
+  }
+
+//TEST CASES
+console.log(defineSuit('Q♠'))
+console.log(defineSuit('9♦'))
+console.log(defineSuit('J♥'))
